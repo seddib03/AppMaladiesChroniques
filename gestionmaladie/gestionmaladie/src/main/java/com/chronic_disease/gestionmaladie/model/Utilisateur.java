1523f99cb974669803054e11f1dea6e68012851b
Salma Eddib <s.eddibsalma@gmail.com>
2025-05-10 16:26:30 +0100

    creation de l'entite utilisateur

---
+++ gestionmaladie/gestionmaladie/src/main/java/com/chronic_disease/gestionmaladie/model/Utilisateur.java
@@ -0,0 +1,65 @@
+package com.chronic_disease.gestionmaladie.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utilisateur {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
+    private String sexe;
+    private String maladie;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	public String getMaladie() {
+		return maladie;
+	}
+	public void setMaladie(String maladie) {
+		this.maladie = maladie;
+	}
+    
+    
+
+}
---
+++ gestionmaladie/gestionmaladie/src/test/java/com/chronic_disease/gestionmaladie/GestionmaladieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronic_disease.gestionmaladie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionmaladieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
